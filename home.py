48be78f8a7c952d9d3796ea46443b7e0fe4ba781
dygy1111-netizen <dygy1111@gmail.com>
2025-09-19 10:08:59 +0900

    Update home.py

--- home.py
+++ home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="위험물탱크 E-매뉴얼", page_icon="🛢️")
 
-st.title("🛢️ 클릭하며 배우는 위험물탱크 E-매뉴얼")
+st.title("위험물탱크 E-매뉴얼")
 st.markdown("""
 👈 **왼쪽 상단의 파란 ▤ 메뉴(사이드바)** 를 클릭해  
 **E-매뉴얼** 또는 **자주하는 질문(FAQ)** 페이지로 이동하세요.
