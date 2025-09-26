0b245747f16393baaaa7ee2abb5fd7cb43f72d82
dygy1111-netizen <dygy1111@gmail.com>
2025-09-26 13:20:53 +0900

    Update home.py

--- home.py
+++ home.py
@@ -82,7 +82,6 @@ html, body, [class*="css"] {
 # ======================= 사이드바 ======================= #
 with st.sidebar:
     st.header("📂 빠른 메뉴")
-    st.markdown("---")
     # ✅ 대제목 → 하위 메뉴 펼침
     for main, subs in sections.items():
         with st.expander(f"📂 {main}", expanded=False):
